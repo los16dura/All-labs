ed310b48fa3fcb7b02165f61d46967884f6384e4
los16dura <Ulechka_1999@mail.ru>
2017-03-17 10:39:17 +0400

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,13 +1,1 @@
-def choice_sort(C):
-    for i in range(0,len(C)-1):
-        for k in range(i+1,len(C)):
-            if C[k] < C[i]:
-                C[k],C[i] = C[i],C[k]
-
-    return(C)
-
-A = input().split()
-B = input().split()
-C = A+B
-choice_sort(C)
-print(' '.join(list(map(str,C))))
+import sys
